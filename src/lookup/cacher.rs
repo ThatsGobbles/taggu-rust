caff5a5724e6a95c830cb30d976029ac56e6933c
Mark LeMoine <thatsgobbles@gmail.com>
2018-01-26 12:07:11 -0800

    Moved lookup to dir mod hierarchy

---
+++ src/lookup/cacher.rs
@@ -0,0 +1,119 @@
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
+
+use error::*;
+use metadata::MetaBlock;
+use library::Library;
+
+pub type MetadataCache = HashMap<PathBuf, MetaBlock>;
+pub type MetaFileCache = HashMap<PathBuf, MetadataCache>;
+
+pub struct LookupCacher<'a> {
+    cache: MetaFileCache,
+    media_lib: &'a Library,
+}
+
+impl<'a> LookupCacher<'a> {
+    pub fn new(media_lib: &'a Library) -> Self {
+        LookupCacher {
+            cache: hashmap![],
+            media_lib,
+        }
+    }
+
+    pub fn cache_meta_files<I, P>(&mut self, meta_fps: I, force: bool) -> Result<()>
+    where I: IntoIterator<Item = P>,
+          P: AsRef<Path>,
+    {
+        for meta_fp in meta_fps.into_iter() {
+            let meta_fp = meta_fp.as_ref();
+
+            // Check if the entry is already cached, and skip if cache request is not forced.
+            if !force && self.cache.contains_key(meta_fp) {
+                continue;
+            }
+
+            // Remove the old entry from the cache.
+            // TODO: Create .remove_cached_meta_file().
+            let _ = self.cache.remove(meta_fp);
+
+            // Temporary metadata cache, filled in below.
+            let mut temp: MetadataCache = hashmap![];
+
+            for (item_fp, meta_block) in self.media_lib.item_fps_from_meta_fp(meta_fp)? {
+                temp.insert(item_fp, meta_block);
+            }
+
+            self.cache.insert(meta_fp.to_path_buf(), temp);
+        }
+
+        Ok(())
+    }
+
+    pub fn cache_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P, force: bool) -> Result<()> {
+        self.cache_meta_files(&[meta_fp], force)
+    }
+
+    pub fn cache_item_files<I, P>(&mut self, item_fps: I, force: bool) -> Result<()>
+    where I: IntoIterator<Item = P>,
+          P: AsRef<Path>,
+    {
+        for item_fp in item_fps.into_iter() {
+            let item_fp = item_fp.as_ref();
+
+            // Get the meta files that could provide info for this item.
+            // TODO: Remove duplicates.
+            let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
+
+            self.cache_meta_files(&meta_fps, force)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn cache_item_file<P: AsRef<Path>>(&mut self, item_fp: P, force: bool) -> Result<()> {
+        self.cache_item_files(&[item_fp], force)
+    }
+
+    pub fn clear(&mut self) -> Result<()> {
+        self.cache.clear();
+        Ok(())
+    }
+
+    pub fn clear_meta_files<I, P>(&mut self, meta_fps: I) -> Result<()>
+    where I: IntoIterator<Item = P>,
+          P: AsRef<Path>,
+    {
+        for meta_fp in meta_fps.into_iter() {
+            let meta_fp = meta_fp.as_ref();
+            let _ = self.cache.remove(meta_fp);
+        }
+
+        Ok(())
+    }
+
+    pub fn clear_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P) -> Result<()> {
+        self.clear_meta_files(&[meta_fp])
+    }
+
+    pub fn clear_item_files<I, P>(&mut self, item_fps: I) -> Result<()>
+    where I: IntoIterator<Item = P>,
+          P: AsRef<Path>,
+    {
+        for item_fp in item_fps.into_iter() {
+            let item_fp = item_fp.as_ref();
+
+            // Get the meta files that could provide info for this item.
+            // TODO: Remove duplicates.
+            let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
+
+            self.clear_meta_files(&meta_fps)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn clear_item_file<P: AsRef<Path>>(&mut self, item_fp: P) -> Result<()> {
+        self.clear_item_files(&[item_fp])
+    }
+}
--- src/lookup/mod.rs
+++ src/lookup/mod.rs
@@ -1,3 +1,5 @@
+pub mod cacher;
+
 use std::path::{Path, PathBuf};
 use std::collections::{HashSet, HashMap};
 
@@ -63,190 +65,77 @@ impl LookupOptions {
     }
 }
 
-pub type MetadataCache = HashMap<PathBuf, MetaBlock>;
-pub type MetaFileCache = HashMap<PathBuf, MetadataCache>;
+// pub struct LookupContext<'a> {
+//     media_lib: &'a Library,
+//     cache: MetaFileCache,
+// }
 
-pub struct LookupCacher<'a> {
-    cache: MetaFileCache,
-    media_lib: &'a Library,
-}
+// impl<'a> LookupContext<'a> {
+//     pub fn new(media_lib: &'a Library) -> LookupContext<'a> {
+//         LookupContext {
+//             media_lib,
+//             cache: hashmap![],
+//         }
+//     }
 
-impl<'a> LookupCacher<'a> {
-    pub fn new(media_lib: &'a Library) -> Self {
-        LookupCacher {
-            cache: hashmap![],
-            media_lib,
-        }
-    }
+//     fn cache_meta_files<I, P>(&mut self, meta_fps: I, force: bool) -> Result<()>
+//     where I: IntoIterator<Item = P>,
+//           P: AsRef<Path>,
+//     {
+//         for meta_fp in meta_fps.into_iter() {
+//             let meta_fp = meta_fp.as_ref();
 
-    pub fn cache_meta_files<I, P>(&mut self, meta_fps: I, force: bool) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for meta_fp in meta_fps.into_iter() {
-            let meta_fp = meta_fp.as_ref();
+//             // Check if the entry is already cached, and skip if cache request is not forced.
+//             if !force && self.cache.contains_key(meta_fp) {
+//                 continue;
+//             }
 
-            // Check if the entry is already cached, and skip if cache request is not forced.
-            if !force && self.cache.contains_key(meta_fp) {
-                continue;
-            }
+//             // Remove the old entry from the cache.
+//             // TODO: Create .remove_cached_meta_file().
+//             let _ = self.cache.remove(meta_fp);
 
-            // Remove the old entry from the cache.
-            // TODO: Create .remove_cached_meta_file().
-            let _ = self.cache.remove(meta_fp);
+//             // Temporary metadata cache, filled in below.
+//             let mut temp: MetadataCache = hashmap![];
 
-            // Temporary metadata cache, filled in below.
-            let mut temp: MetadataCache = hashmap![];
+//             for (item_fp, meta_block) in self.media_lib.item_fps_from_meta_fp(meta_fp)? {
+//                 temp.insert(item_fp, meta_block);
+//             }
 
-            for (item_fp, meta_block) in self.media_lib.item_fps_from_meta_fp(meta_fp)? {
-                temp.insert(item_fp, meta_block);
-            }
+//             self.cache.insert(meta_fp.to_path_buf(), temp);
+//         }
 
-            self.cache.insert(meta_fp.to_path_buf(), temp);
-        }
+//         Ok(())
+//     }
 
-        Ok(())
-    }
+//     fn cache_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P, force: bool) -> Result<()> {
+//         self.cache_meta_files(&[meta_fp], force)
+//     }
 
-    pub fn cache_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P, force: bool) -> Result<()> {
-        self.cache_meta_files(&[meta_fp], force)
-    }
+//     fn cache_item_files<I, P>(&mut self, item_fps: I, force: bool) -> Result<()>
+//     where I: IntoIterator<Item = P>,
+//           P: AsRef<Path>,
+//     {
+//         for item_fp in item_fps.into_iter() {
+//             let item_fp = item_fp.as_ref();
 
-    pub fn cache_item_files<I, P>(&mut self, item_fps: I, force: bool) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for item_fp in item_fps.into_iter() {
-            let item_fp = item_fp.as_ref();
+//             // Get the meta files that could provide info for this item.
+//             // TODO: Remove duplicates.
+//             let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
 
-            // Get the meta files that could provide info for this item.
-            // TODO: Remove duplicates.
-            let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
+//             self.cache_meta_files(&meta_fps, force)?;
+//         }
 
-            self.cache_meta_files(&meta_fps, force)?;
-        }
+//         Ok(())
+//     }
 
-        Ok(())
-    }
+//     fn cache_item_file<P: AsRef<Path>>(&mut self, item_fp: P, force: bool) -> Result<()> {
+//         self.cache_item_files(&[item_fp], force)
+//     }
 
-    pub fn cache_item_file<P: AsRef<Path>>(&mut self, item_fp: P, force: bool) -> Result<()> {
-        self.cache_item_files(&[item_fp], force)
-    }
-
-    pub fn clear(&mut self) -> Result<()> {
-        self.cache.clear();
-        Ok(())
-    }
-
-    pub fn clear_meta_files<I, P>(&mut self, meta_fps: I) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for meta_fp in meta_fps.into_iter() {
-            let meta_fp = meta_fp.as_ref();
-            let _ = self.cache.remove(meta_fp);
-        }
-
-        Ok(())
-    }
-
-    pub fn clear_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P) -> Result<()> {
-        self.clear_meta_files(&[meta_fp])
-    }
-
-    pub fn clear_item_files<I, P>(&mut self, item_fps: I) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for item_fp in item_fps.into_iter() {
-            let item_fp = item_fp.as_ref();
-
-            // Get the meta files that could provide info for this item.
-            // TODO: Remove duplicates.
-            let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
-
-            self.clear_meta_files(&meta_fps)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn clear_item_file<P: AsRef<Path>>(&mut self, item_fp: P) -> Result<()> {
-        self.clear_item_files(&[item_fp])
-    }
-}
-
-pub struct LookupContext<'a> {
-    media_lib: &'a Library,
-    cache: MetaFileCache,
-}
-
-impl<'a> LookupContext<'a> {
-    pub fn new(media_lib: &'a Library) -> LookupContext<'a> {
-        LookupContext {
-            media_lib,
-            cache: hashmap![],
-        }
-    }
-
-    fn cache_meta_files<I, P>(&mut self, meta_fps: I, force: bool) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for meta_fp in meta_fps.into_iter() {
-            let meta_fp = meta_fp.as_ref();
-
-            // Check if the entry is already cached, and skip if cache request is not forced.
-            if !force && self.cache.contains_key(meta_fp) {
-                continue;
-            }
-
-            // Remove the old entry from the cache.
-            // TODO: Create .remove_cached_meta_file().
-            let _ = self.cache.remove(meta_fp);
-
-            // Temporary metadata cache, filled in below.
-            let mut temp: MetadataCache = hashmap![];
-
-            for (item_fp, meta_block) in self.media_lib.item_fps_from_meta_fp(meta_fp)? {
-                temp.insert(item_fp, meta_block);
-            }
-
-            self.cache.insert(meta_fp.to_path_buf(), temp);
-        }
-
-        Ok(())
-    }
-
-    fn cache_meta_file<P: AsRef<Path>>(&mut self, meta_fp: P, force: bool) -> Result<()> {
-        self.cache_meta_files(&[meta_fp], force)
-    }
-
-    fn cache_item_files<I, P>(&mut self, item_fps: I, force: bool) -> Result<()>
-    where I: IntoIterator<Item = P>,
-          P: AsRef<Path>,
-    {
-        for item_fp in item_fps.into_iter() {
-            let item_fp = item_fp.as_ref();
-
-            // Get the meta files that could provide info for this item.
-            // TODO: Remove duplicates.
-            let mut meta_fps = self.media_lib.meta_fps_from_item_fp(&item_fp)?;
-
-            self.cache_meta_files(&meta_fps, force)?;
-        }
-
-        Ok(())
-    }
-
-    fn cache_item_file<P: AsRef<Path>>(&mut self, item_fp: P, force: bool) -> Result<()> {
-        self.cache_item_files(&[item_fp], force)
-    }
-
-    fn clear_cache(&mut self) {
-        self.cache.clear();
-    }
-}
+//     fn clear_cache(&mut self) {
+//         self.cache.clear();
+//     }
+// }
 
 pub type LookupResult = Result<Option<MetaValue>>;
 
