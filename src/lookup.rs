2552a76e0c07c50ba3d29cfa39ac175745ad2abf
Mark LeMoine <thatsgobbles@gmail.com>
2018-01-29 12:32:30 -0800

    Removed separate cacher submodule

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,5 +1,3 @@
-pub mod cacher;
-
 use std::path::{Path, PathBuf};
 use std::collections::{HashSet, HashMap};
 
@@ -7,7 +5,9 @@ use library::Library;
 use helpers::normalize;
 use metadata::{MetaValue, MetaBlock};
 use error::*;
-use self::cacher::{LookupCacher, MetadataCache, MetaFileCache};
+
+pub type MetadataCache = HashMap<PathBuf, MetaBlock>;
+pub type MetaFileCache = HashMap<PathBuf, MetadataCache>;
 
 trait LabelExtractor {
     fn extract_label<S: AsRef<str>>(&self, item_file_name: S) -> String;
@@ -412,12 +412,14 @@ pub type LookupResult = Result<Option<MetaValue>>;
 
 #[cfg(test)]
 mod tests {
+    use std::path::{Path, PathBuf};
+    use std::collections::HashSet;
     use std::thread::sleep;
     use std::time::Duration;
 
     use tempdir::TempDir;
 
-    // use super::{lookup_origin, lookup_parents, lookup_children, LookupOptions};
+    use super::{LookupContext, MetadataCache, MetaFileCache};
     use library::{Library, LibraryBuilder};
     use library::selection::Selection;
     use metadata::MetaValue;
@@ -505,4 +507,210 @@ mod tests {
 
 //         // let produced = lookup_children(&media_lib, tp.join("ALBUM_01"), &LookupOptions::new("const_key")).unwrap();
 //     }
+
+    fn extract_all_meta_fps(raw_cache: &MetaFileCache) -> HashSet<PathBuf> {
+        raw_cache.keys().into_iter().cloned().collect()
+    }
+
+    fn extract_all_item_fps(raw_cache: &MetaFileCache) -> HashSet<PathBuf> {
+        raw_cache
+            .values()
+            .into_iter()
+            .flat_map(|item_fp_to_mb| item_fp_to_mb.keys())
+            .cloned()
+            .collect()
+    }
+
+    fn extract_sub_item_fps<P: AsRef<Path>>(raw_cache: &MetaFileCache, meta_fp: P) -> HashSet<PathBuf> {
+        let meta_fp = meta_fp.as_ref();
+
+        raw_cache
+            .get(meta_fp)
+            .expect("key not found in cache")
+            .keys()
+            .into_iter()
+            .cloned()
+            .collect()
+    }
+
+    enum EqualityTarget {
+        AllMetas,
+        AllItems,
+        SubItems(PathBuf),
+    }
+
+    fn test_fp_set_equality(expected_fps: HashSet<PathBuf>, raw_cache: &MetaFileCache, target: EqualityTarget) {
+        let produced_fps = match target {
+            EqualityTarget::AllMetas => extract_all_meta_fps(raw_cache),
+            EqualityTarget::AllItems => extract_all_item_fps(raw_cache),
+            EqualityTarget::SubItems(ref meta_fp) => extract_sub_item_fps(raw_cache, &meta_fp),
+        };
+
+        assert_eq!(expected_fps, produced_fps);
+    }
+
+    #[test]
+    fn test_new() {
+        let (temp_media_root, media_lib) = default_setup("test_new");
+        let tp = temp_media_root.path();
+
+        let mut lookup_ctx = LookupContext::new(&media_lib);
+
+        assert!(lookup_ctx.cache.is_empty());
+    }
+
+    #[test]
+    fn test_cache_meta_file() {
+        let (temp_media_root, media_lib) = default_setup("test_cache_meta_file");
+        let tp = temp_media_root.path();
+
+        let mut lookup_ctx = LookupContext::new(&media_lib);
+
+        let meta_fp = tp.join("ALBUM_01").join("item.yml");
+        lookup_ctx.cache_meta_file(&meta_fp, false).expect("unable to cache meta file");
+
+        let expected_meta_fps = hashset![
+            tp.join("ALBUM_01").join("item.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01").join("DISC_01"),
+            tp.join("ALBUM_01").join("DISC_02"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_sub_item_fps(&lookup_ctx.cache, &meta_fp);
+        assert_eq!(expected_item_fps, produced_item_fps);
+
+        let meta_fp = tp.join("ALBUM_01").join("item.yml");
+        lookup_ctx.cache_meta_file(&meta_fp, false).expect("unable to cache meta file");
+
+        let expected_meta_fps = hashset![
+            tp.join("ALBUM_01").join("item.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01").join("DISC_01"),
+            tp.join("ALBUM_01").join("DISC_02"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_sub_item_fps(&lookup_ctx.cache, &meta_fp);
+        assert_eq!(expected_item_fps, produced_item_fps);
+
+        let meta_fp = tp.join("ALBUM_01").join("self.yml");
+        lookup_ctx.cache_meta_file(&meta_fp, false).expect("unable to cache meta file");
+
+        let expected_meta_fps = hashset![
+            tp.join("ALBUM_01").join("item.yml"),
+            tp.join("ALBUM_01").join("self.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_sub_item_fps(&lookup_ctx.cache, &meta_fp);
+        assert_eq!(expected_item_fps, produced_item_fps);
+    }
+
+    #[test]
+    fn test_cache_item_file() {
+        let (temp_media_root, media_lib) = default_setup("test_cache_item_file");
+        let tp = temp_media_root.path();
+
+        let mut lookup_ctx = LookupContext::new(&media_lib);
+
+        let item_fp = tp.join("ALBUM_01").join("DISC_01");
+        lookup_ctx.cache_item_file(&item_fp, false).expect("unable to cache item file");
+
+        let expected_meta_fps = hashset![
+            tp.join("ALBUM_01").join("item.yml"),
+            tp.join("ALBUM_01").join("DISC_01").join("self.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01").join("DISC_01"),
+            // All item files pointed to by the item's meta file are cached.
+            tp.join("ALBUM_01").join("DISC_02"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_all_item_fps(&lookup_ctx.cache);
+        assert_eq!(expected_item_fps, produced_item_fps);
+
+        let item_fp = tp.join("ALBUM_01").join("DISC_02");
+        lookup_ctx.cache_item_file(&item_fp, false).expect("unable to cache item file");
+
+        let expected_meta_fps = hashset![
+            // This should already be present from the first lookup.
+            tp.join("ALBUM_01").join("item.yml"),
+            tp.join("ALBUM_01").join("DISC_01").join("self.yml"),
+            tp.join("ALBUM_01").join("DISC_02").join("self.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01").join("DISC_01"),
+            // All item files pointed to by the item's meta file are cached.
+            tp.join("ALBUM_01").join("DISC_02"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_all_item_fps(&lookup_ctx.cache);
+        assert_eq!(expected_item_fps, produced_item_fps);
+
+        let item_fp = tp.join("ALBUM_01").join("DISC_01").join("TRACK_01.flac");
+        lookup_ctx.cache_item_file(&item_fp, false).expect("unable to cache item file");
+
+        let expected_meta_fps = hashset![
+            // This should already be present from the first lookup.
+            tp.join("ALBUM_01").join("item.yml"),
+            tp.join("ALBUM_01").join("DISC_01").join("self.yml"),
+            tp.join("ALBUM_01").join("DISC_01").join("item.yml"),
+            tp.join("ALBUM_01").join("DISC_02").join("self.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        let expected_item_fps = hashset![
+            tp.join("ALBUM_01").join("DISC_01"),
+            // All item files pointed to by the item's meta file are cached.
+            tp.join("ALBUM_01").join("DISC_01").join("TRACK_01.flac"),
+            tp.join("ALBUM_01").join("DISC_01").join("TRACK_02.flac"),
+            tp.join("ALBUM_01").join("DISC_01").join("TRACK_03.flac"),
+            tp.join("ALBUM_01").join("DISC_02"),
+        ];
+        let produced_item_fps: HashSet<PathBuf> = extract_all_item_fps(&lookup_ctx.cache);
+        assert_eq!(expected_item_fps, produced_item_fps);
+    }
+
+    #[test]
+    fn test_clear() {
+        let (temp_media_root, media_lib) = default_setup("test_clear");
+        let tp = temp_media_root.path();
+
+        let mut lookup_ctx = LookupContext::new(&media_lib);
+
+        lookup_ctx.cache_item_file(tp.join("ALBUM_01"), false).expect("unable to cache item file");
+        lookup_ctx.cache_item_file(tp.join("ALBUM_02"), false).expect("unable to cache item file");
+        lookup_ctx.cache_item_file(tp.join("ALBUM_03"), false).expect("unable to cache item file");
+        lookup_ctx.cache_item_file(tp.join("ALBUM_05"), false).expect("unable to cache item file");
+
+        let expected_meta_fps = hashset![
+            tp.join("item.yml"),
+            tp.join("ALBUM_01").join("self.yml"),
+            tp.join("ALBUM_02").join("self.yml"),
+            tp.join("ALBUM_03").join("self.yml"),
+            tp.join("ALBUM_05").join("self.yml"),
+        ];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+
+        lookup_ctx.clear().expect("unable to clear cache");
+
+        let expected_meta_fps = hashset![];
+        let produced_meta_fps: HashSet<PathBuf> = extract_all_meta_fps(&lookup_ctx.cache);
+        assert_eq!(expected_meta_fps, produced_meta_fps);
+    }
 }
